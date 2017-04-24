b30121f224e10d0129b1d848a6c0bd01fd72377d
Dan McHenry <danpmch@gmail.com>
2017-04-23 23:17:41 -0400

    Removed all the logic for adjusting start angle via the pot from gong driver

--- gong.ino
+++ gong.ino
@@ -3,26 +3,17 @@
 
 Servo myServo;
 
-const int potPin = A0;
 const int firePin = 12;
 
 const int STRIKE_ANGLE = 80;
 const int MAX_START_ANGLE = 0;
 const int TRAVEL_TIME = 120;
 
-int lastPotValue;
-int currentPotValue;
-int currentMaxAngle;
-
 void setup() {
   myServo.attach(9);
   myServo.write(MAX_START_ANGLE);
 
   Serial.begin(9600);
-
-  int initValue = analogRead(potPin);
-  lastPotValue = initValue + 5;
-  currentPotValue = initValue;
 
   pinMode(firePin, INPUT);
 }
@@ -31,21 +22,11 @@ void fire()
 {
   myServo.write(STRIKE_ANGLE);
   delay(TRAVEL_TIME);
-  myServo.write(currentMaxAngle);
+  myServo.write(MAX_START_ANGLE);
   delay(TRAVEL_TIME);
 }
 
 void loop() {
-  currentPotValue = analogRead(potPin);
-
-  if( abs(currentPotValue - lastPotValue) > 4 )
-  {
-    lastPotValue = currentPotValue;
-    currentMaxAngle = map(currentPotValue, 0, 1023, STRIKE_ANGLE, MAX_START_ANGLE);
-    myServo.write(currentMaxAngle);
-  }
-
-  delay(15);
 
   if(Serial.available())
   {
